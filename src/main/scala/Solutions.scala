cefc7ab38c0ecec73603238e3164367bca029af2
Edi Weissmann <edi.weissmann@gmail.com>
2012-07-08 22:47:13 +0200

    Problem 18

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -3,6 +3,54 @@ package net.projecteuler.ediweissmann
 import annotation.tailrec
 
 /*
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+   3
+  7 4
+ 2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below
+ */
+object P18 extends Solvable {
+
+  def solve() = {
+
+    val in = """75
+               95 64
+             17 47 82
+            18 35 87 10
+           20 04 82 47 65
+          19 01 23 75 03 34
+         88 02 77 73 07 63 67
+        99 65 04 28 06 16 70 92
+       41 41 26 56 83 40 80 70 33
+      41 48 72 33 47 32 37 16 94 29
+     53 71 44 65 25 43 91 52 97 51 14
+    70 11 33 28 77 73 17 78 39 68 17 57
+   91 71 52 38 17 14 91 43 58 50 27 29 48
+  63 66 04 68 89 53 67 30 73 16 69 87 40 31
+ 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+      .lines.map(_.trim.split(" ").toSeq.map(_.toInt)).toSeq
+
+    /**
+     * Given the maximum routes for the previous row and the next row, computes the maximum routes for next row
+     */
+    def nextMax(lastRowMax: Seq[Seq[Int]], nextRow: Seq[Int]): Seq[Seq[Int]] = {
+      nextRow.zipWithIndex.map({
+        case (value, index) => {
+          (lastRowMax.lift(index - 1) ++ lastRowMax.lift(index)).flatten.map(_ + value).toSeq
+        }
+      })
+    }
+
+    in.tail.foldLeft(Seq(in.head))(nextMax(_, _)).flatten.max
+  }
+}
+
+/*
 If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
